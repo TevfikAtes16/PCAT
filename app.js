9c677029b43074c1ff2e7cbfbd5ebfa3c9a2d7c5
TevfikAtes16 <tevfikates1@gmail.com>
2023-10-10 15:41:12 +0300

    Template ejs hazırlandı.

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const path = require('path');
-
-const app = express();
-
-//Middlewares
-app.use(express.static('public'));
+const ejs = require('ejs');
 
 const port = 3000;
+const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './temp/index.html'));
+  // res.sendFile(path.resolve(__dirname, './temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 app.listen(port, () => {
